65923b75c79c8b1d06f0463cbf2dd37d0fe5e8a1
Bhavani <bhavanipandian1511@gmail.com>
2024-06-13 20:13:13 +0530

    final commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const textareaE1 = document.getElementById("textarea")
+const totalcounterE1 = document.getElementById("total-counter")
+const remainingcounterE1 = document.getElementById("remaining-counter")
+
 textareaE1.addEventListener("keyup", ()=>{
     updateCounter()
 })
 
+updateCounter()
+
 function updateCounter(){
-    
+    totalcounterE1.innerText = textareaE1.value.length
+    remainingcounterE1.innerText = textareaE1.getAttribute("maxlength") - textareaE1.value.length;
 }
+
